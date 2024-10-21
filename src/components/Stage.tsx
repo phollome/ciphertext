04c66c318007609b78640036a982ee74958f76ac
Peter Holló <hello@songsforthe.dev>
2024-10-21 18:22:39 +0200

    fix: typo

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -6,7 +6,10 @@ function Stage() {
         className="font-thin text-xl px-4 py-2 rounded-lg bg-slate-600 text-slate-50 outline-none focus:border-2"
         placeholder="insert text"
       />
-      <p id="cyphertext" className="break-all min-h-8 min-w-4 text-center font-thin text-2xl text-slate-50 border-b"></p>
+      <p
+        id="ciphertext"
+        className="break-all min-h-8 min-w-4 text-center font-thin text-2xl text-slate-50 border-b"
+      ></p>
     </div>
   );
 }
